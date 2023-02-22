a3b76a73af4278e109b9b8f7b968876854b970c3
Muhaddis, Muhammad <muhaddisshah@gmail.com>
2023-02-22 13:31:24 +0500

    feat(add): add create alias command

--- src/create-alias.tsx
+++ src/create-alias.tsx
@@ -1,6 +1,18 @@
-import { showHUD, showToast, Toast, getPreferenceValues, List, ActionPanel, Clipboard, Action } from "@raycast/api";
-import fetch, { FormData } from "node-fetch";
+import {
+  showHUD,
+  openExtensionPreferences,
+  showToast,
+  Detail,
+  Toast,
+  getPreferenceValues,
+  ActionPanel,
+  Clipboard,
+  Action,
+  Form,
+} from "@raycast/api";
+import fetch from "node-fetch";
 import { useEffect, useState } from "react";
+import { fetchAccont } from "./utils";
 
 interface Preferences {
   api_token: string;
@@ -13,11 +25,24 @@ interface Domain {
 
 interface State {
   domains?: Domain[];
-  error?: Error;
+  error?: string;
+  forwardingEmail?: string;
+  aliasError?: string;
+  domainError?: string;
+  isLoading: false,
+  forwardingEmailError: string;
 }
 
-export default function Command() {
-  const [state, setState] = useState<State>({}),
+export default function createAlias() {
+  const [state, setState] = useState<State>({
+      domains: undefined,
+      error: "",
+      forwardingEmail: "",
+      aliasError: "", 
+      domainError: "",
+      forwardingEmailError: "",
+      isLoading: false,
+    }),
     API_TOKEN = getPreferenceValues<Preferences>().api_token,
     API_URL = "https://api.improvmx.com/v3/";
 
@@ -34,77 +59,161 @@ export default function Command() {
         });
 
         if (!apiResponse.ok) {
-          throw new Error(`Fetch failed with status ${apiResponse.status}: ${apiResponse.statusText}`);
+          if (apiResponse.status === 401) {
+            setState((prevState) => {
+              return { ...prevState, error: "Invalid API Token" };
+            });
+
+            return;
+          }
         }
 
-        const response = await apiResponse.json() as unknown;
+        const response = (await apiResponse.json()) as unknown;
         const domains = response as { domains: Array<Domain> };
 
-        setState({ domains: domains.domains });
-
-      } catch (error) {
-        setState({
-          error: error instanceof Error ? error : new Error("Something went wrong"),
+        setState((prevState) => {
+          return { ...prevState, domains: domains.domains, error: "" };
         });
+      } catch (error) {
+        setState((prevState) => {
+          return { ...prevState, error: "Failed to fetch domains. Please try again later." };
+        });
+        return;
       }
     }
 
+    async function forwardingEmailFn() {
+      const email = await fetchAccont(auth, API_URL);
+      setState({ ...state, forwardingEmail: email });
+      setState((prevState) => {
+        return { ...prevState, forwardingEmail: email };
+      });
+    }
+
     getDomains();
+    forwardingEmailFn();
   }, []);
 
-return (
-    <List isLoading={state.domains === undefined} searchBarPlaceholder="Filter domains..." isShowingDetail>
-      <List.Section title="Domains">
-        {state.domains?.map((domain: Domain) => (
-          <List.Item
-            key={domain.display}
-            title={domain.display}
-            icon={{ value: domain.banned || domain.active == false ? "🔴" : "🟢", source: "emoji" }}
-            actions={
-              <ActionPanel>
-                <Action
-                  title="Create a Masked Email Address"
-                  onAction={() => {
+  const showError = async () => {
+    if (state.error) {
+      await showToast(Toast.Style.Failure, state.error);
+    }
+  };
 
-                    if (domain.banned || domain.active == false) {
-                      showToast(Toast.Style.Failure, "Domain is banned or inactive");
-                      return;
-                    }
+  const handleSumbit = async (values : any)  => {
+     const { domain, alias } = values;
+    const aliasError = alias.length === 0 ? "Alias is required" : "";
+    const domainError = domain.length === 0 ? "Domain is required" : "";
+    const forwardingEmailError = state.forwardingEmail?.length === 0 ? "Forwarding Email is required" : "";
 
-                    const form = new FormData();
-                    form.append(
-                      "alias",
-                      Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-                    );
-                    form.append("forward", "muhaddisshah@gmail.com");
 
-                    fetch(API_URL + `domains/${domain.display}/aliases/`, {
-                      method: "POST",
-                      headers: {
-                        Authorization: "Basic " + auth,
-                      },
-                      body: form,
-                    })
-                      .then((response) => response.json())
-                      .then(async (data : any ) => {
-                        await Clipboard.copy(data.alias.alias + "@" + domain.display);
-                        await showHUD("Masked email created successfully " + data.alias.alias + "@" + domain.display + " and copied to clipboard");
+    if (aliasError || domainError || forwardingEmailError)  {
+      setState((prevState) => {
+        return { ...prevState, aliasError, domainError, forwardingEmailError };
+      });
+      showToast(Toast.Style.Failure, "Please fill out all required fields");
+      return;
+    }
 
-                      })
-                      .catch(async (error) => {
-                        await showToast({
-                          style: Toast.Style.Failure,
-                          title: error.message,
-                        })
-                      });
-                  }}
-                />
-              </ActionPanel>
-            }
-            detail={<List.Item.Detail markdown={"Create masked email using **" + domain.display + "**"} />}
-          />
-        ))}
-      </List.Section>
-    </List>
+    setState((prevState : any) => {
+      return { ...prevState, isLoading: true };
+    });
+
+    const formData = {
+      "forward": state.forwardingEmail,
+      "alias": alias,
+    }
+
+    try {
+
+      const apiResponse = await fetch(API_URL + "domains/" + domain + "/aliases", {
+        method: "POST",
+        headers: {
+          Authorization: "Basic " + auth,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      
+      if (await !apiResponse.ok) {
+
+        if (apiResponse.status === 401) {
+          setState((prevState) => {
+            return { ...prevState, error: "Invalid API Token", isLoading: false };
+          });
+
+          await showToast(Toast.Style.Failure, "Error", "Invalid API Token");
+         
+          return;
+        }
+        
+        const response = (await apiResponse.json()) as unknown;
+        const errors = response as { errors: { alias: Array<string> } };
+        const error = errors.errors.alias[0];
+
+        setState((prevState) => {
+          return { ...prevState, error: error, isLoading: false };
+        });
+
+        await showToast(Toast.Style.Failure, "Error", error);
+        return;
+      }
+    } catch (error) {
+      return;
+    }
+
+    await showToast(Toast.Style.Success, "Success", "Alias created successfully " +  alias + "@" + domain);
+    await Clipboard.copy(alias + "@" + domain);
+    await showHUD("Alias copied to clipboard " + alias + "@" + domain);
+    
+    
+  }
+
+  showError();
+
+  return state.error ? (
+    <Detail
+      markdown={"⚠️" + state.error}
+      actions={
+        <ActionPanel>
+          <Action title="Open Extension Preferences" onAction={openExtensionPreferences} />
+        </ActionPanel>
+      }
+    />
+  ) : (
+    <Form isLoading={state.domains === undefined || state.isLoading}
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Create Alias" onSubmit={(values) => handleSumbit(values)} />
+        </ActionPanel>
+      }
+
+    >
+      <Form.Dropdown id="domain" title="Domain" placeholder="Select a domain"
+        error={state.domainError}
+      >
+        {state.domains
+          ?.filter((domain) => !domain.banned && domain.active)
+          .map((domain) => (
+            <Form.Dropdown.Item key={domain.display} value={domain.display} title={domain.display} />
+          ))}
+      </Form.Dropdown>
+
+      <Form.TextField
+        id="alias"
+        title="Alias"
+        placeholder="Enter an alias"
+        error={state.aliasError}
+      />
+      <Form.TextField
+        id="forwardingEmail"
+        title="Forwarding Email"
+        placeholder="Enter a forwarding email"
+        value={state.forwardingEmail}
+        error={state.forwardingEmailError}
+        onChange={(value) => setState({ ...state, forwardingEmail: value })}
+      />
+    </Form>
   );
 }
